9bed3a7e172add2875d9cf8fbbfa8a3bac8798eb
Petra Barus <petra.barus@gmail.com>
2019-12-28 10:40:08 +0700

    docs: Fix README to explain the tutorial.

--- cdk/app.ts
+++ cdk/app.ts
@@ -1,23 +1,37 @@
 #!/usr/bin/env node
+/*********************************
+ * AWS CDK script to provision the resources.
+ */
+
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import ecs = require("@aws-cdk/aws-ecs");
 import ecsPatterns = require("@aws-cdk/aws-ecs-patterns");
 
 class WebECSCluster extends cdk.Construct {
+
     constructor(scope: cdk.Construct) {
         super(scope, "WebECSCluster");
 
         const ecsCluster = new ecs.Cluster(this, 'ecsCluster');
-        new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Service', {
+        const fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Service', {
             cluster: ecsCluster,
             taskImageOptions: {
                 image: ecs.ContainerImage.fromAsset('.')
             },
         });
+
+        const autoscaling = fargateService.service.autoScaleTaskCount({
+            minCapacity: 2,
+            maxCapacity: 4
+        });
+        autoscaling.scaleOnCpuUtilization('CpuScaling', {
+            targetUtilizationPercent: 50,
+            scaleInCooldown: cdk.Duration.seconds(60),
+            scaleOutCooldown: cdk.Duration.seconds(60),
+        });
     }
 }
-
 
 class WebStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
